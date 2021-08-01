ae058677748a689470000a1f622b9c4901a78b96
3104kdy <3104kdy@naver.com>
2021-08-01 21:49:24 +0900

    [ADD] Need to set the content and organize

---
+++ lib/home/home.dart
@@ -0,0 +1,26 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:an_muk_so/camera/camera_page.dart';
+
+import 'package:an_muk_so/services/db.dart';
+
+//import 'package:an_muk_so/models/drug.dart';
+import 'package:an_muk_so/models/user.dart';
+
+//import 'package:an_muk_so/shared/image.dart';
+
+import 'package:an_muk_so/theme/colors.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
