2cee55c47a65bc2d992293e28fef0026aea38906
3104kdy <3104kdy@naver.com>
2021-08-01 21:40:55 +0900

    [ADD] Theme color organized

---
+++ lib/theme/colors.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const gradient_button_start = Color(0xFF4BBEB3);
+const gradient_button_end = Color(0xFF58C9B8);
+const gradient_button_long_start = Color(0xFF6FDBCC);
+const gradient_button_long_end = Color(0xFF4BC2B6);
+const gradient_button_inactivated_start = Color(0xFFD9DBDB);
+const gradient_button_inactivated_end = Color(0xFFC0C2C2);
+
+const gradient_top_start = Color(0xFFE8FFFB);
+const gradient_top_middle = Color(0x38D2FCF5);
+const gradient_top_end = Color(0xFFFFFFFF);
+
+const primary700 = Color(0xFF027B6D);
+const primary600_bold_text = Color(0xFF00A48A); //진한 text
+const primary500_light_text = Color(0xFF00B39C); //연한 text
+const primary400_line = Color(0xFF00C2AE);
+const primary300_main = Color(0xFF66D5C6);
+const primary200 = Color(0xFF85F2E4);
+const primary100 = Color(0xFFA7E5DC);
+const primary50 = Color(0xFFDCF5F2);
+
+const gray900 = Color(0xFF0D0D0D);
+const gray800 = Color(0xFF1F1F1F);
+const gray750_activated = Color(0xFF2C2C2C);
+const gray700 = Color(0xFF454545);
+const gray600 = Color(0xFF666666);
+const gray500 = Color(0xFF898989);
+const gray400 = Color(0xFF979999);
+const gray300_inactivated = Color(0xFFB1B3B3);
+const gray200 = Color(0xFFCACCCC);
+const gray100 = Color(0xFFD9D9D9);
+const gray75 = Color(0xFFE4E5E6);
+const gray50 = Color(0xFFF7F7F7);
+const gray0_white = Color(0xFFFFFFFF);
+
+const black87 = Color(0x87000000);
+const black70 = Color(0x70000000);
+const black55 = Color(0x55000000);
+const black40 = Color(0x40000000);
+const black15 = Color(0x15000000);
+const black5 = Color(0x05000000);
+
+const warning = Color(0xFFFF7070);
+const yellow = Color(0xFFFED74D);
+const yellow_line = Color(0xFFFFCB13);
