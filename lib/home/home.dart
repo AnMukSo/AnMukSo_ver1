b5697240e9a5fb0f752d84624eccf12be1b2a41a
3104kdy <3104kdy@naver.com>
2021-09-02 23:16:41 +0900

    [UPDATE] Delete camera dart

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,4 @@
-import 'package:camera/camera.dart';
+//import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
